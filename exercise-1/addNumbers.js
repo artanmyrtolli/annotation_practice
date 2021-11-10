72751b92e30575749565d641c2738babd1481090
Artan Myrtolli <artanmyrtolli@gmail.com>
2021-11-09 21:42:59 -0500

    Add annotations to addnumbers.js

--- exercise-1/addNumbers.js
+++ exercise-1/addNumbers.js
@@ -1,20 +1,34 @@
+//declare a function called addNumbers that takes no parameters
 function addNumbers() {
-  var firstNum = 9
-  var secondNum = 14
+  //declare a variable named firstNum, give it value of 9
+  var firstNum = 9;
+  //declare a variable named secondNum, give it value of 14
+  var secondNum = 14;
 
-  firstNum = 3
-  secondNum = 4
+//reassign variable firstNum to value of 3
+  firstNum = 3;
+  //reassign variable secondNum to value of 4
+  secondNum = 4;
 
-  var nums = [firstNum, secondNum]
+//declaring variable nums to string array with elements of variables
+//firstNum and secondNum
+  var nums = [firstNum, secondNum];
 
-  var product = firstNum * secondNum
-  var sum = firstNum + secondNum
-  var average = sum / nums.length
-
-  console.log(nums[0])
-  console.log(nums[1])
-
-  console.log(`The first number is ${num1} and the second number is ${num2}!`)
-
-  return sum
+//declare new variable called product, value is varible firstNum multiplied
+//by variable secondNum
+  var product = firstNum * secondNum;
+  //declare new variable called product, value is varible firstNum added
+  //to variable secondNum
+  var sum = firstNum + secondNum;
+  //declare a new variable called average. It takes variable sum and
+  //divides it by the length of the nums array.
+  var average = sum / nums.length;
+//log to console index 0 of array nums. Expected "3"
+  console.log(nums[0]);
+  //log to console index 1 of array nums. Expected "4"
+  console.log(nums[1]);
+//interpolate variables num1 and num2 into string
+  console.log(`The first number is ${num1} and the second number is ${num2}!`);
+//when functino is called will give vale of variable sum.
+  return sum;
 }
